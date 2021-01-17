2a5da21e7771533a84d7f1cefd71ff92e7e16bb2
Elisa Chen <skechen@gmail.com>
2021-01-17 16:45:25 +0100

    minor styling improvements to navbar

--- src/components/NavMain.jsx
+++ src/components/NavMain.jsx
@@ -3,6 +3,7 @@ import { NavLink } from "react-router-dom";
 import { withUser } from "../components/Auth/withUser";
 import apiHandler from "../api/apiHandler";
 
+import AppBar from "@material-ui/core/AppBar";
 import SearchIcon from "@material-ui/icons/Search";
 import NotificationsIcon from "@material-ui/icons/Notifications";
 import Avatar from "@material-ui/core/Avatar";
@@ -11,6 +12,8 @@ import Menu from "@material-ui/core/Menu";
 import MenuItem from "@material-ui/core/MenuItem";
 import Link from "@material-ui/core/Link";
 import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+
 import { makeStyles } from "@material-ui/core/styles";
 
 import "../styles/NavMain.css";
@@ -56,70 +59,78 @@ const NavMain = (props) => {
   console.log(context);
 
   return (
-    <nav className="NavMain">
-      <NavLink exact to={context.isLoggedIn ? "/dashboard" : "/"}>
-        <h3 className="logo">YASHA</h3>
-      </NavLink>
-      <ul className="nav-list">
-        {context.isLoggedIn && (
-          <React.Fragment>
-            <Grid container justify="center" className={classes.root}>
-              <Grid>
-                {/* <Button color="primary"> */}
-                <SearchIcon></SearchIcon>
-                {/* </Button> */}
-              </Grid>
-              <Grid>
-                {/* <Button color="primary"> */}
-                <NotificationsIcon></NotificationsIcon>
-                {/* </Button> */}
-              </Grid>
-              <Grid>
-                <Button
-                  aria-controls="simple-menu"
-                  aria-haspopup="true"
-                  size="small"
-                  onClick={handleClick}
-                >
-                  {/* {context.user && ( */}
-                  <Avatar
-                    alt={context.user.firstName}
-                    src={context.user.avatar}
-                    className={classes.small}
-                  ></Avatar>
-                </Button>
-                {/* )} */}
-                <Menu
-                  id="simple-menu"
-                  anchorEl={anchorEl}
-                  keepMounted
-                  open={Boolean(anchorEl)}
-                  onClose={handleClose}
-                >
-                  <MenuItem onClick={handleClose}>
-                    <Link href="/profile">My user profile</Link>
-                  </MenuItem>
-                  <MenuItem onClick={handleClose}>
-                    <Link href="/company">My company profile</Link>
-                  </MenuItem>
-                  <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                </Menu>
-              </Grid>
-            </Grid>
-          </React.Fragment>
-        )}
-        {!context.isLoggedIn && (
-          <React.Fragment>
-            <li>
-              <NavLink to="/signin">Log in</NavLink>
-            </li>
-            {/* <li>
+    <AppBar position="static">
+      <Grid container alignItems="center" justify="space-between" id="navbar">
+        <Grid item>
+          <NavLink exact to={context.isLoggedIn ? "/dashboard" : "/"}>
+            <Typography variant="h6">
+              <strong>YASHA</strong>
+            </Typography>
+          </NavLink>
+        </Grid>
+        <Grid item>
+          <ul className="nav-list">
+            {context.isLoggedIn && (
+              <React.Fragment>
+                <Grid container justify="center" className={classes.root}>
+                  <Grid>
+                    {/* <Button color="primary"> */}
+                    <SearchIcon></SearchIcon>
+                    {/* </Button> */}
+                  </Grid>
+                  <Grid>
+                    {/* <Button color="primary"> */}
+                    <NotificationsIcon></NotificationsIcon>
+                    {/* </Button> */}
+                  </Grid>
+                  <Grid>
+                    <Button
+                      aria-controls="simple-menu"
+                      aria-haspopup="true"
+                      size="small"
+                      onClick={handleClick}
+                    >
+                      {/* {context.user && ( */}
+                      <Avatar
+                        alt={context.user.firstName}
+                        src={context.user.avatar}
+                        className={classes.small}
+                      ></Avatar>
+                    </Button>
+                    {/* )} */}
+                    <Menu
+                      id="simple-menu"
+                      anchorEl={anchorEl}
+                      keepMounted
+                      open={Boolean(anchorEl)}
+                      onClose={handleClose}
+                    >
+                      <MenuItem onClick={handleClose}>
+                        <Link href="/profile">My user profile</Link>
+                      </MenuItem>
+                      <MenuItem onClick={handleClose}>
+                        <Link href="/company">My company profile</Link>
+                      </MenuItem>
+                      <MenuItem onClick={handleLogout}>Logout</MenuItem>
+                    </Menu>
+                  </Grid>
+                </Grid>
+              </React.Fragment>
+            )}
+            {!context.isLoggedIn && (
+              <React.Fragment>
+                <li>
+                  <NavLink to="/signin">Log in</NavLink>
+                </li>
+                {/* <li>
               <NavLink to="/signup">Create account</NavLink>
             </li> */}
-          </React.Fragment>
-        )}
-      </ul>
-    </nav>
+              </React.Fragment>
+            )}
+          </ul>
+        </Grid>
+      </Grid>
+    </AppBar>
   );
 };
 
