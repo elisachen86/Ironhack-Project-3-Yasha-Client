3c6b05916e6338175cb601aeba8bcf97de686334
Marine Sanjuan <marine@sanjuan.eu>
2021-01-20 01:11:25 +0100

    workeround - style date typed inputs in formNewOrder.css

--- src/components/CardOrderStatus.jsx
+++ src/components/CardOrderStatus.jsx
@@ -14,7 +14,6 @@ import Box from "@material-ui/core/Box";
 import Popover from "@material-ui/core/Popover";
 import PopupState, { bindTrigger, bindPopover } from "material-ui-popup-state";
 import Loading from "./Loading";
-import Check from "@material-ui/icons/Check";
 
 // PLUGINS REQUIRED TO FORMAT THE DATE
 const dayjs = require("dayjs");
