5877cf2b0f182a259f9e3b2587688e8363af9d12
Marine Sanjuan <marine@sanjuan.eu>
2021-01-20 11:04:27 +0100

    cleaned formShipped and formReceived a bit

--- src/components/Forms/FormReceived.jsx
+++ src/components/Forms/FormReceived.jsx
@@ -1,29 +1,43 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
-import Loading from '../Loading';
+import Loading from "../Loading";
 
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
 
+import CloudUploadSharpIcon from "@material-ui/icons/CloudUploadSharp";
+
+import "../../styles/formReceived.css";
+
 export class FormReceived extends Component {
-
   render() {
-
     // console.log("Form Received - Props", this.props);
 
     return this.props !== null ? (
-      <Container component="main" maxWidth="xs" className="auth-container">
-        <div>
-          <Typography
-            align="center"
-            component="h1"
-            variant="h5"
-            gutterBottom="true"
-          >
-            Has your order arrived?
-          </Typography>
+      <Container component="main" maxWidth="xs" className="received-container">
+        <div className="received-card">
+          <div className="received-item">
+            <img
+              src="/images/receive-pic.png"
+              alt="Shipment-illustration"
+              className="ship-pic"
+            />
+          </div>
+
+          <div className="received-item">
+            <Typography
+              align="center"
+              component="h1"
+              variant="h5"
+              gutterBottom="true"
+              className="received-title"
+            >
+              Has your order arrived?
+            </Typography>
+          </div>
+
           <form
             onSubmit={this.props.handleSubmit}
             onChange={this.props.handleChange}
@@ -33,16 +47,19 @@ export class FormReceived extends Component {
               <Button
                 variant="raised"
                 component="span"
-                // variant="contained"
+                variant="contained"
                 color="secondary"
                 size="small"
-                className="upload-btn"
+                className="upload-btn received-item"
               >
+                <span className="upload-icon">
+                  <CloudUploadSharpIcon></CloudUploadSharpIcon>
+                </span>
                 Upload reception documents
               </Button>
             </label>
 
-            <Box className="button-box">
+            <Box className="button-box received-item">
               <Button
                 type="submit"
                 fullWidth
--- src/components/Forms/FormShipped.jsx
+++ src/components/Forms/FormShipped.jsx
@@ -1,31 +1,38 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
 
-import Loading from '../Loading';
+import Loading from "../Loading";
 
 import Button from "@material-ui/core/Button";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
 import Box from "@material-ui/core/Box";
+import CloudUploadSharpIcon from "@material-ui/icons/CloudUploadSharp";
+
+import "../../styles/formShipped.css";
 
 export class FormShipped extends Component {
-  
   render() {
-
-   // console.log("Form Shipped - props", this.props);
+    // console.log("Form Shipped - props", this.props);
 
     return this.props !== null ? (
-      <Container component="main" maxWidth="xs" className="auth-container">
-        <div>
-          <Typography
-            align="center"
-            component="h1"
-            variant="h5"
-            gutterBottom="true"
-          >
+      <Container component="main" maxWidth="xs" className="shipped-container">
+        <div className="shipped-card">
 
-                Is your order ready to ship?
-          </Typography>
+        {/* FOR PROJECT PRESENTATION PURPOSES - DO NOT KEEP THIS IMG
+        CREDITS: https://dribbble.com/shots/14966911-FICHA-illustrations */}
+        <div className="shipped-item"><img src="/images/shipment-pic.png" alt="Shipment-illustration" className="ship-pic"/></div>
+          <div className="shipped-item">
+            <Typography
+              align="center"
+              component="h1"
+              variant="h5"
+              gutterBottom="true"
+              className="shipped-title"
+            >
+              Is your order ready to ship?
+            </Typography>
+          </div>
           <form
             onSubmit={this.props.handleSubmit}
             onChange={this.props.handleChange}
@@ -38,13 +45,16 @@ export class FormShipped extends Component {
                 variant="contained"
                 color="secondary"
                 size="small"
-                className="upload-btn"
+                className="upload-btn shipped-item"
               >
+                <span className="upload-icon">
+                  <CloudUploadSharpIcon></CloudUploadSharpIcon>
+                </span>
                 Upload shipping documents
               </Button>
             </label>
 
-            <Box className="button-box">
+            <Box className="button-box shipped-item">
               <Button
                 type="submit"
                 fullWidth
